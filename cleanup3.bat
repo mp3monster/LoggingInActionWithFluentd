10cdd1e9301926153bd4a23b00073dbc3151362a
Phil Wilkins <phil@mp3monster.org>
2020-05-11 20:47:48 +0100

    Added solution

--- cleanup3.bat
+++ cleanup3.bat
@@ -1,7 +1,7 @@
 cd ./Chapter3
 del fluentd-file-output.*.log
 del structured-rolling-log.*.log
-del rotating-file-read.pos_file
+del *.pos_file
 del basic-file.*
 del fluentd-file-output.*.gz
 rmdir fluentd-file-output 
