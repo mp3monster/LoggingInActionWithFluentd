1d73620cbfb787d3c13f015c5f07055abd85472e
Phil Wilkins <phil@mp3monster.org>
2020-05-11 17:49:33 +0100

    Update cleanup3.bat
    updated to provide better deletion - takes into account the log files

--- cleanup3.bat
+++ cleanup3.bat
@@ -2,6 +2,7 @@ cd ./Chapter3
 del fluentd-file-output.*.log
 del structured-rolling-log.*.log
 del rotating-file-read.pos_file
+del basic-file.*
 del fluentd-file-output.*.gz
 rmdir fluentd-file-output 
 
