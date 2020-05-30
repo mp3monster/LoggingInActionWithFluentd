e709a91ab268c46ff6d24b553bd2de59f404b7f9
Phil Wilkins <phil@mp3monster.org>
2020-05-30 23:30:04 +0100

    configuration and data etc for demo a c6 example
    configuration and data etc for demo a c6 example

---
+++ cleanup6.bat
@@ -0,0 +1,11 @@
+cd ./Chapter6
+del annotated-file-*.txt
+del *.pos_file
+del *.log
+del basic-file*.txt
+
+rmdir fluentd-file-output  
+rmdir label-pipeline-file-output  
+rmdir nonlabel-file-output   
+
+cd ..
