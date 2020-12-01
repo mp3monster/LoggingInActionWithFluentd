19864e98ab4f8365e93fb7026d54aee1b7faa617
Phil Wilkins <phil@mp3monster.org>
2020-12-01 02:02:46 +0000

    Development of example plugin

--- Chapter8/fluent-plugin-out-redislist/lib/fluent/plugin/out_redislist.rb
+++ Chapter8/fluent-plugin-out-redislist/lib/fluent/plugin/out_redislist.rb
@@ -17,47 +17,85 @@ require "fluent/plugin/output"
 require "redis"
 # we need the Redis gem to help
 
-#module Fluent
-#  module Plugin
+module Fluent
+  module Plugin
     class RedislistOutput < Fluent::Plugin::Output
       Fluent::Plugin.register_output("redislist", self)
 
-      config_param :portNo, :integer, default: 6379, :secret false
-      config_param :hostAddr, :string, default: "localhost", secret: false; alias: host_address
-      config_param :listName, :string, default: "fluentd"
+      config_param :port, :integer, default: 6379, secret: false, alias: :port
+      config_param :hostaddr, :string, default: "127.0.0.1", secret: false
+      config_param :listname, :string, default: "fluentd"
 
       # use the Redis connection as a member variable so we don't need to keep constructing connections
       # each time we need to perform an operation
-      @redisConnection :Redis
+      @redis
       # We could enhance connection handling with:
       # - provide the option to include username and password
       # - HTTPS connectivity
       # - connection pooling
       # - specify connection timeouts and reconnection behavior
 
-      def process(tag, es)
-        buildConnection
-        @redisConnection.multi do
+      def process(tag,es)
+        if !@redis
+          connect_redis()
+        end
+
+        log.debug "Request received"
+
+        @redis.multi do
           es.each do |time, record|
-            $stdout.write redisConnection.lpush (listName, buildListEntry(tag, time, record))
+            log.debug time
+            log.debug record
+            if (@redis.connected?)
+              newlength = @redis.lpush(@listname,"value")
+              log.info "lpushed to "+@listname
+            else
+              log.warn "no connection to Redis"
+            end
           end
         end
       end
 
-      # create the Redis connection only if the connection is nil
-      def buildConnection()
-        if @redisConnection.nil 
-          @redisConnection = Redis.new (host:hostAddr, port:portno)
-        end
+      def configure(conf)
+        super
+        @port = conf['port']
+        # add custom validation here If the configuration is invalid, raise Fluent::ConfigError
       end
+
+      def start
+        super
+        log.debug "starting redis plugin\n"
+        connect_redis()
+      end
+
+      def shutdown
+        super
+        if @redis 
+          @redis.disconnect!
+          log.debug "disconnecting from redis\n"
+        end
+      end      
 
       # combine the Fluentd tag, timestamp and record into a JSON payload
       # as we don't knowe for sure whether the record will be JSON in nature - easiest if we process
       # everything as a string.
-      def buildListEntry (tag, time, record)
-        return '{"tag" : '+ tag + ",time" : ' + time + ', "record" :'+record+'"}'
+      def buildListEntry(tag,time,record)
+        return '{"tag" : '+ tag + '",time" : ' + time + ', "record" :'+record+'"}'
+      end
+
+      def connect_redis()
+        if !@redis
+          begin
+          #@redis=Redis.new(host:@hostaddr,port:@port)
+          @redis=Redis.new(host:"127.0.0.1",port:6379,connect_timeout:5,reconnect_attempts:2)
+          @redis.connect!
+          log.debug "Connected to Redis "+@redis.connected?.to_s
+          rescue
+            log.error "Error connecting to redis"
+          end
+        end
       end
 
     end
-#  end
-# end
+  end
+ end
