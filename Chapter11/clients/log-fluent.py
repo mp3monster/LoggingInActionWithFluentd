7364ddb4864487f40be7fbd9b3e1723b0932c406
Phil Wilkins <phil@mp3monster.org>
2021-02-19 23:47:08 +0000

    improvements to the log messages

--- Chapter11/clients/log-fluent.py
+++ Chapter11/clients/log-fluent.py
@@ -5,5 +5,5 @@ from fluent import handler, sender
 
 sender= sender.FluentSender('test', host='localhost', port=18090)
 # using the Fluentd Handler means that msgpack will be used and therefore the source plugin in Fluentd is a forward plugin.
-
-sender.emit_with_time('', int(time.time()), {'from': 'log-fluent', 'at': datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")})
+now = datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+sender.emit_with_time('', int(time.time()), {'from': 'log-fluent', 'at': now})
