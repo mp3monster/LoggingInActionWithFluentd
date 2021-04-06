4c6ec36431a88955aa6c55493f0359550bdd3eb3
Phil Wilkins <phil@mp3monster.org>
2021-04-06 17:31:28 +0100

    fixed missing semi-colons

--- Chapter8/Docker/run.sh
+++ Chapter8/Docker/run.sh
@@ -1,8 +1,8 @@
-apt-get install wget; \
-apt-get install ruby; \
+apt-get install wget -y; \
+apt-get install ruby -y; \
 gem install fluentd; \
-wget https://raw.githubusercontent.com/mp3monster/LoggingInActionWithFluentd/master/Chapter8/Fluentd/file-forward.conf \
-echo file is $FLUENT_SOURCE_PATH \
-echo host is $FLUENT_FOWARD_HOST \
-echo port is $FLUENT_FOWARD_PORT \
+wget https://raw.githubusercontent.com/mp3monster/LoggingInActionWithFluentd/master/Chapter8/Fluentd/file-forward.conf; \
+echo file is $FLUENT_SOURCE_PATH; \
+echo host is $FLUENT_FOWARD_HOST; \
+echo port is $FLUENT_FOWARD_PORT; \
 fluentd -c ./file-forward.conf 
