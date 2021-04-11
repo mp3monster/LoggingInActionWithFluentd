b5275a4eadb599e83088925fe09d71bdc45e0e02
Phil Wilkins <phil@mp3monster.org>
2021-04-11 15:56:12 +0100

    correct path

--- Chapter8/deploy-log-sim-k8.bat
+++ Chapter8/deploy-log-sim-k8.bat
@@ -1,2 +1,2 @@
 minikube kubectl -- delete deployment log-simulator --namespace=default
-minikube kubectl -- apply -f %LogSimulatorHome%\LogGenerator\Kubernetes\log-simulator-deployment.yaml
+minikube kubectl -- apply -f %LogSimulatorHome%/Kubernetes/log-simulator-deployment.yaml --namespace=default
